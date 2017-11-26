b6617920406a4910ae1d471526243ecf1ee0282d
KINGJIN\Administrator <jinhua12110@163.com>
2017-11-26 16:25:44 +0800

    add test

---
+++ test2.py
@@ -0,0 +1,19 @@
+#-*- coding: UTF-8 -*-
+from time import sleep
+
+
+
+#通过get_cookies获取cookie信息
+#from selenium import webdriver
+
+#driver = webdriver.Firefox()
+#driver.get('http://www.foxsaas.com')
+#sleep(4)
+
+#获取cookie信息
+#cookie = driver.get_cookie()
+#print(cookie)
+
+#driver.quit()
+
+print('jinhua')
